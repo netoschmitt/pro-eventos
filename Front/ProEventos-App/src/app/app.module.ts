1dbd8645077526db3d893569ff39b6f68599b9fe
neto schmitt <agneloschmittneto@gmail.com>
2022-05-13 22:05:31 -0300

    Configurando CORS na startup.cs e add metodo getEventos no TS

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
@@ -14,7 +16,8 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
