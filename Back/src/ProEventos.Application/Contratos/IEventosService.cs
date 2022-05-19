c3a3e25be57f4e9ea7610b3cb2a1c5cb577956e3
neto schmitt <agneloschmittneto@gmail.com>
2022-05-19 14:19:06 -0300

    Evento Application

--- Back/src/ProEventos.Application/Contratos/IEventosService.cs
+++ Back/src/ProEventos.Application/Contratos/IEventosService.cs
@@ -11,7 +11,7 @@ namespace ProEventos.Application.Contratos
 
         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
-        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes = false);
+        Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false);
 
     }
 }
