298b1f29f0aee5b790e111e95a7ef10974d88a61
neto schmitt <agneloschmittneto@gmail.com>
2022-05-22 21:07:34 -0300

    Angular Rotas Parte 1

--- Front/ProEventos-App/src/app/nav/nav.component.spec.ts
+++ Front/ProEventos-App/src/app/nav/nav.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     TestBed.configureTestingModule({
       declarations: [ NavComponent ]
     })
