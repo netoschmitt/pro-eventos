3d74aecb2a2450f35874fd2e3648b9d3acf0d333
neto schmitt <agneloschmittneto@gmail.com>
2022-05-24 21:51:51 -0300

    form completo validação e reset/salvar

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -24,10 +24,14 @@ export class EventoDetalheComponent implements OnInit {
       tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
       local: ['', Validators.required],
       dataEvento: ['', Validators.required],
-      qtdPessoas: ['', [Validators.required, Validators.max(12000)]],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
       telefone: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       imagemURL: ['', Validators.required],
     });
+  }
+
+  public resetForm(): void {
+    this.form.reset();
   }
 }
