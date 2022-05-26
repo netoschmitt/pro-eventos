3843cb2f2840d9263f0660963212977201ce811d
neto schmitt <agneloschmittneto@gmail.com>
2022-05-26 19:03:16 -0300

    Confirmar Senha mustMatch

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,5 +1,6 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/validatorField';
 
 @Component({
   selector: 'app-registration',
@@ -12,20 +13,28 @@ export class RegistrationComponent implements OnInit {
 
   constructor(public fb: FormBuilder) { }
 
+  get f(): any { return this.form.controls; }
+
   ngOnInit(): void {
     this.validation();
   }
 
   private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'passwordConfirm')
+    };
+
     this.form = this.fb.group({
-      primeiroNome: ['', Validators.required],
-      ultimonome: ['', Validators.required],
-      email: ['', Validators.required, Validators.email],
-      username: ['', Validators.required],
-      senha: ['',
-      Validators.required,
-      Validators.minLength(6)],
-      confirmeSenha: ['', Validators.required],
-    });
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['',
+      [Validators.required, Validators.email]
+      ],
+      userName: ['', Validators.required],
+      password: ['',
+      [Validators.required, Validators.minLength(6)]
+      ],
+      passwordConfirm: ['', Validators.required],
+    }, formOptions);
   }
 }
