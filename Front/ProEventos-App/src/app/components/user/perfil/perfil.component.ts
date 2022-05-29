506663ad6ff2fd739189b4340847a1d403def32d
neto schmitt <agneloschmittneto@gmail.com>
2022-05-28 22:05:11 -0300

    validação do form componente perfil

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControlOptions } from '@angular/forms';
+import { ValidatorField } from './../../../helpers/validatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  // Conveniente para pegar um FormField apenas com a letra F
+  get f(): any { return this.form.controls; }
+
+  ngOnInit(): void {
+    this.validation();
   }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password' , 'passwordConfirm')
+    };
+
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email] ],
+      fone: ['', Validators.required],
+      function: ['', Validators.required],
+      description: ['', Validators.required],
+      password: ['', [Validators.nullValidator, Validators.minLength(6)]],
+      passwordConfirm: ['', Validators.nullValidator],
+    }, formOptions);
+  }
+
+  onSubmit(): void {
+    // vai parar aqui se o form estiver inválido
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
+  }
+
+
+
 
 }
