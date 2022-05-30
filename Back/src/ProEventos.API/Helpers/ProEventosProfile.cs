d90c45803c45540f3ef62f313a2cd0fe0d2b7e71
neto schmitt <agneloschmittneto@gmail.com>
2022-05-30 00:47:35 -0300

    AutoMapper e Profile

---
+++ Back/src/ProEventos.API/Helpers/ProEventosProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using ProEventos.API.Dtos;
+using ProEventos.Domain;
+
+
+namespace ProEventos.API.Helpers
+{
+    public class ProEventosProfile : Profile
+    {
+        public ProEventosProfile()
+        {
+            CreateMap<Evento, EventoDto>();
+        }
+    }
+}
