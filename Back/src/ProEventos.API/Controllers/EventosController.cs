6506dc2089de43d110f2c17902be7e6b609605a8
neto schmitt <agneloschmittneto@gmail.com>
2022-05-30 21:59:57 -0300

    Mapeando metodos gets

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProEventos.Application.Contratos;
-using ProEventos.Domain;
-using ProEventos.API.Dtos;
+using ProEventos.Application.Dtos;
 
 namespace ProEventos.API.Controllers
 {
@@ -32,25 +30,7 @@ namespace ProEventos.API.Controllers
                 var eventos = await this.eventoService.GetAllEventosAsync(true);
                 // se eventos for null ou nao for encontrado return NotFound...
                 if (eventos == null) return NotFound("Nenhum evento encontrado.");
-                // criei uma listagem desse EventosDTO
-                var eventosRetorno = new List<EventoDto>();
-
-                foreach (var evento in eventos)
-                {
-                    // criada uma nova listagem Dto baseado nos campos abaixo 
-                    eventosRetorno.Add(new EventoDto(){
-                        Id = evento.Id,
-                        Local = evento.Local,
-                        DataEvento = evento.DataEvento.ToString(),
-                        Tema = evento.Tema,
-                        QtdPessoas = evento.QtdPessoas,
-                        ImagemURL = evento.ImagemURL,
-                        Telefone = evento.Telefone,
-                        Email = evento.Email,
-                    });
-                }
-                // agora eu tenho um dto sendo retornado,assim nao estou exponho exatamente aquilo que o meu doninio tem, para aquele que consumir minha api 
-                return Ok(eventosRetorno);
+                return Ok(eventos);
             }
             catch (Exception ex)
             {
@@ -94,7 +74,7 @@ namespace ProEventos.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Evento model)
+        public async Task<IActionResult> Post(EventoDto model)
         {
            try
             {
@@ -111,7 +91,7 @@ namespace ProEventos.API.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, Evento model)
+        public async Task<IActionResult> Put(int id, EventoDto model)
         {
             try
             {
