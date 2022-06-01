edce504770099cc0bfb24a776abad94afa5fa973
neto schmitt <agneloschmittneto@gmail.com>
2022-06-01 09:07:18 -0300

    Validaçao E-mail, QTDPessoas, telefone

--- Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -15,11 +15,18 @@ namespace ProEventos.Application.Dtos
         StringLength(50, MinimumLength = 3, ErrorMessage = "Intervalo permitido de 3 a 50 caracteres.")]
         public string Tema { get; set; }
 
+        [Display(Name ="Qtd Pessoas")]
+        [Range(1, 120000, ErrorMessage ="{0} não pode ser menor que 1 e maior que 120.000")]
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigátorio")]
+        [Phone(ErrorMessage ="O campo {0} está com numero inválido")]
         public string Telefone { get; set; }
 
-        [EmailAddress(ErrorMessage ="O campo {0} precisa ser um campo válido")]
+        [Required(ErrorMessage ="O campo {0} é obrigatório.")]
+        [Display(Name = "e-mail")]
+        [EmailAddress(ErrorMessage ="O necessário ser um {0} válido")]
         public string Email { get; set; }
 
     
