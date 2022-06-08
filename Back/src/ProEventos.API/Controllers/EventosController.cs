1578319db377cfa19545541ad7f4666167f6aa5a
neto schmitt <agneloschmittneto@gmail.com>
2022-06-08 18:29:51 -0300

    Metodo Salvar imagem

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -89,7 +90,7 @@ namespace ProEventos.API.Controllers
                 if (file.Length > 0)
                 {
                     DeleteImage(evento.ImagemURL);
-                    // evento.ImagemURL = SaveImage(file);
+                    evento.ImagemURL = await SaveImage(file);
                 }
                 var EventoRetorno = await this.eventoService.UpdateEvento(eventoId, evento);
 
@@ -157,11 +158,30 @@ namespace ProEventos.API.Controllers
 
         }
 
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName)
+                                            .Take(10)
+                                            .ToArray()
+                                            ).Replace(' ', '-');
+
+            imageName = $"{imageName}{DateTime.UtcNow.ToString("yymmssfff")}{Path.GetExtension(imageFile.FileName)}";
+
+            var imagePath = Path.Combine(hostEnvironment.ContentRootPath, @"Resources/images", imageName);
+
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            return imageName;
+        }
         /* nao é um endpoint */
         [NonAction]
-        public void DeleteIMG(string imageName)
+        public void DeleteImage(string imageName)
         {
-            var imagePath = Path.Combine(hostEnvironment.ContentRootPath, @"Resouces/images", imageName);
+            var imagePath = Path.Combine(hostEnvironment.ContentRootPath, @"Resources/images", imageName);
             if (System.IO.File.Exists(imagePath))
                 System.IO.File.Delete(imagePath);
         }
