d3812b719b9b41d64035e86ff5e3a965c0cf6517
neto schmitt <agneloschmittneto@gmail.com>
2022-06-11 17:44:08 -0300

    Herança do Contexto

--- Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
+++ Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProEventos.Domain;
+using ProEventos.Domain.Identity;
 
 namespace ProEventos.Persistence.Contextos
 {
-    public class ProEventosContext : DbContext
+    public class ProEventosContext : IdentityDbContext<IdentityDbContext<User, Role, int,
+                                                                        IdentityUserClaim<int>,
+                                                                        IdentityUserRole<int>,
+                                                                        IdentityUserLogin<int>, 
+                                                                        IdentityRoleClaim<int>, 
+                                                                        IdentityUserToken<int>>
     {
         public ProEventosContext(DbContextOptions<ProEventosContext> options)
          : base(options) { }
